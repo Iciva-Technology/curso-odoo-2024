82b44ff39a5dece9ce30cd59322be2b6eebea74b
Arnorwer <estebanavila322@gmail.com>
2024-12-16 12:13:43 -0400

    mi primer commit

--- barbershop/models/barbershop_appointment.py
+++ barbershop/models/barbershop_appointment.py
@@ -1,5 +1,7 @@
-from odoo import models, fields
-
+from odoo import models, fields, api
+import logging
+_logger = logging.getLogger(__name__)
+from datetime import datetime, timedelta
 
 class BarbershopAppointment(models.Model):
     _name = "barbershop.appointment"
@@ -7,7 +9,22 @@ class BarbershopAppointment(models.Model):
 
     name = fields.Char(string="Referencia")
     start_at = fields.Datetime(string="Desde", required=True)
-    end_at = fields.Datetime(string="Hasta", required=True)
+    end_at = fields.Datetime(string="Hasta")
     partner_id = fields.Many2one('res.partner', string="Cliente")
     state_id = fields.Many2one('barbershop.state', string="Estado")
     service_ids = fields.Many2many('barbershop.service', string="Servicios")
+    amount_services = fields.Float(string="Monto de servicios", compute="_compute_amount_services")
+    barber_id = fields.Many2one('hr.employee', string="Barbero")
+
+    @api.depends('service_ids')
+    def _compute_amount_services(self):
+        for rec in self:
+            for item in rec.service_ids:
+                rec.amount_services += item.price
+
+    def prueba(self):
+        for rec in self:
+            total = 0
+            for item in rec.service_ids:
+                total += item.price
+                _logger.info(f'\n\n\n\n\n {total} \n\n\n\n\n\n')
--- barbershop/models/barbershop_service.py
+++ barbershop/models/barbershop_service.py
@@ -7,8 +7,11 @@ class BarbershopService(models.Model):
 
     name = fields.Char(string="Nombre", required=True)
     description = fields.Html(string="Descripción")
-    duration = fields.Integer(string="Duración", help="Duración del servicio expresado en minutos")
+    duration = fields.Float(string="Duración", help="Duración del servicio expresado en minutos")
     number_of_appointments = fields.Integer(string="Número de citas", compute="_compute_number_of_appointments")
+    number_of_tasks = fields.Integer(string="Número de tareas", compute="_compute_number_of_tasks")
+    counts_appoinments_ids = fields.Many2many('barbershop.appointments', string="Citas")
+    counts_tasks_ids = fields.Many2many('barbershop.tasks', string="Tareas")
     price = fields.Float(string="Precio")
     difficulty = fields.Selection([
         ('easy', 'Fácil'),
@@ -17,6 +20,7 @@ class BarbershopService(models.Model):
     image = fields.Binary(string="Imagen")
     aftercare = fields.Html(string="Cuidados posteriores")
 
+    @api.depends('counts_appoinments_ids')
     def _compute_number_of_appointments(self):
         for rec in self:
             appointment_ids = self.env['barbershop.appointment'].search([('service_ids', 'in', rec.id)])
@@ -28,3 +32,16 @@ class BarbershopService(models.Model):
         action = self.env["ir.actions.actions"]._for_xml_id("barbershop.barbershop_appointment_action")
         action['domain'] = [('service_ids', '=', active_id)]
         return action
+    
+    @api.depends('counts_tasks_ids')
+    def _compute_number_of_tasks(self):
+        for rec in self:
+            tasks_ids = self.env['barbershop.tasks'].search([('realted_service_ids', 'in', rec.id)])
+            rec.number_of_tasks = len(tasks_ids)
+
+    def action_tasks_id(self):
+        self.ensure_one()
+        tasks_active_id = self.env.context.get('tasks_active_id')
+        tasks_action = self.env["ir.actions.actions"]._for_xml_id("barbershop.action_barbershop_tasks")
+        tasks_action['domain'] = [('realted_service_ids', '=', tasks_active_id)]
+        return tasks_action
