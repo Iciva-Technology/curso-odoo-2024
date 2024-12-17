0e17beb11974b27cd8cc544bbd301f35b4b0facc
SoyKenMendoza <ken.alemendoza@gmail.com>
2024-12-17 11:59:47 -0400

    Agregar datos computados

--- barbershop/models/barbershop_appointment.py
+++ barbershop/models/barbershop_appointment.py
@@ -1,5 +1,5 @@
 from odoo import models, fields, api
-
+from datetime import timedelta
 
 class BarbershopAppointment(models.Model):
     _name = "barbershop.appointment"
@@ -7,7 +7,7 @@ class BarbershopAppointment(models.Model):
 
     name = fields.Char(string="Referencia", required=True)
     start_at = fields.Datetime(string="Desde", required=True)
-    end_at = fields.Datetime(string="Hasta", required=True)
+    end_at = fields.Datetime(string="Hasta", required=False, compute="_compute_end_at", readonly=True)
     partner_id = fields.Many2one('res.partner', string="Cliente", required=True)
     state_id = fields.Many2one('barbershop.state', string="Estado", required=True)
     employee_id = fields.Many2one('hr.employee', string="Empleado", required=True)
@@ -19,4 +19,14 @@ class BarbershopAppointment(models.Model):
         for appointment in self:
             total = sum(service.price for service in appointment.service_ids)
             appointment.total_price = total
+
+    @api.depends('service_ids')
+    def _compute_end_at(self):
+        for appointment in self:
+            if appointment.start_at:
+                total_duration = sum(service.duration for service in appointment.service_ids)
+                appointment.end_at = appointment.start_at + timedelta(minutes=total_duration)
+            else: 
+                appointment.end_at = None   
+
 
