15fceec6b3acb9347b58edffa4d29392ed9e4082
alfonzojosue <16-00436@usb.ve>
2024-12-18 15:03:55 -0400

    change

--- barbershop/models/barbershop_appointment.py
+++ barbershop/models/barbershop_appointment.py
@@ -1,4 +1,5 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 
 class BarbershopAppointment(models.Model):
@@ -11,3 +12,27 @@ class BarbershopAppointment(models.Model):
     partner_id = fields.Many2one('res.partner', string="Cliente")
     state_id = fields.Many2one('barbershop.state', string="Estado")
     service_ids = fields.Many2many('barbershop.service', string="Servicios")
+
+    @api.model
+    def create(self, vals):
+        record = super(BarbershopAppointment, self).create(vals)
+        record.check_appointment_overlap()
+        return record
+
+    def write(self, vals):
+        res = super(BarbershopAppointment, self).write(vals)
+        res.check_appointment_overlap()
+        return res
+
+    def check_appointment_overlap(self):
+        for rec in self:
+            overlapping_appointments = self.env['barbershop.appointment'].search([
+                ('partner_id', '=', rec.partner_id.id),
+                ('id', '!=', rec.id),  # Excluir la misma cita si es edición
+                ('start_at', '<', rec.end_at),
+                ('end_at', '>', rec.start_at),
+            ])
+            if overlapping_appointments:
+                raise ValidationError('El cliente ya tiene una cita en el rango de tiempo seleccionado.')
+
+
